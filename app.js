2b1fdaa4b601450d3309a807029b6a40d8f6f1c5
ezrogha <rtimbiti@gmail.com>
2020-04-04 21:48:53 +0300

    Add registration functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser')
 
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
@@ -8,6 +9,9 @@ const users = require('./routes/api/users')
 const dbUrl = require("./config/keys").MongoURL;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => res.send("Hola"));
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,49 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs')
+
+const router = express.Router();
+
+const User = require("../../models/User");
 
 // @route  GET api/users/test
 // @desc   Test users endpoint
 // access  Public
 
-router.get('/test', (req, res) => res.json({ msg: "Users work" }))
+router.get("/test", (req, res) => res.json({ msg: "Users work" }));
 
-module.exports = router
+// @route  GET api/users/register
+// @desc   Register User
+// access  Public
+
+router.post("/register", (req, res) => {
+  const { name, email, password } = req.body
+  User.findOne({ email }).then(user => {
+    if (user) {
+      res.status(400).json({ email: "User with email already exists" });
+    } else {
+      const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' })
+      const newUser = new User({
+        name,
+        email,
+        password,
+        avatar
+      })
+
+      bcrypt.genSalt(10, (error, salt) => {
+        bcrypt.hash(newUser.password, salt, (error, hash) => {
+          if(error) throw error
+          newUser.password = hash
+          newUser.save()
+          .then(user => {
+            res.json(user)
+          })
+          .catch(error => console.log(error))
+        })
+      })
+
+    }
+  });
+});
+
+module.exports = router;
